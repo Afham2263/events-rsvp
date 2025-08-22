3ba379e5dbdf6f1fa60ab0d0a71c397260865dc9
Afham2263 <126801750+Afham2263@users.noreply.github.com>
2025-08-22 22:56:23 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,12 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
+
+module.exports = nextConfig
